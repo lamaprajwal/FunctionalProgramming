af686f5749c1c4a836489079b41c64f40ec523ce
unknown <prajwallama28@gmail.com>
2024-06-12 21:48:43 +0545

    New files added

---
+++ cons/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class FuncExtensions
+{
+    public static Func<T2, T1, R> Swap<T1, T2, R>(this Func<T1, T2, R> f)
+        => (t2, t1) => f(t1, t2);
+}
+
+class Program
+{
+    static void Main()
+    {
+        Func<int, int, int> divide = (x, y) => x / y;
+        var divideBy = divide.Swap();
+
+        int result = divideBy(2, 6); // Internally calls divide(6, 2)
+        Console.WriteLine(result); // Output: 3
+    }
+}
+
+
+
+
---
+++ firstClassFunc/Program.cs
@@ -0,0 +1,9 @@
+﻿// assign function to variable isEven
+Func<int, bool> isEven = x => x % 2 == 0;
+var list = Enumerable.Range(1, 10);
+// pass isEven as an argument to Where
+var even = list.Where(isEven).ToList();
+foreach (var item in even)
+{
+    Console.WriteLine(item);
+}
